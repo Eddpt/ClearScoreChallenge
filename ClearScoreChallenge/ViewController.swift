66d40a484b0f4d1b9fc7c6c419fe52005274e0c0
Eddpt <Eddpt@users.noreply.github.com>
2018-04-08 19:54:08 +0100

    Clean up unnecessary methods

--- ClearScoreChallenge/ViewController.swift
+++ ClearScoreChallenge/ViewController.swift
@@ -1,19 +1,7 @@
-//  Copyright © 2018 Schibsted. All rights reserved.
+//  Copyright © 2018 Edgar Antunes. All rights reserved.
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+final class ViewController: UIViewController {
 }
 
